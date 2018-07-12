a000a2e5c2850040c0c3ecb48586c30d0f8eadf2
jswtraveler <40544835+jswtraveler@users.noreply.github.com>
2018-07-12 15:19:57 -0400

    Update string.py

--- string.py
+++ string.py
@@ -1,7 +1,7 @@
 def string_90(df):
     ninety_unique=pd.DataFrame(columns=["Col_Name", "Ninety", "Total"])
-
-    for col in df.select_dtypes(include='object').columns.values:
+#    for col in df.select_dtypes(include='object').columns.values:
+    for col in df.select_dtypes(include=[object]).columns.values:
         freq = df[col].value_counts().cumsum()/df[col].value_counts().sum()
         ninety=freq[freq<=.9].count()
         total=df[col].value_counts().sum()
